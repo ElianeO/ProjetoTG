18f0072ea687467bc50c6e5ff6cc76c9ee3c8614
ElianeO <elianecristina_d@yahoo.com>
2023-05-10 16:22:48 -0300

    (10/05) Conclusão do primeiro CRUD

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 const { Router } = require('express')
 
 const UserController = require('./controllers/UserController')
+const User = require('./models/User')
 
 const router = Router()
 
 router.post('/user-create', UserController.createUser)
+router.put('/user-update/:id', UserController.updateUser)
+router.get('/user-list/', UserController.listUsers)
+router.delete('/user-delete/:id', UserController.deleteUser)
+router.get('/list-one/:id', UserController.listOne)
 
 module.exports = router
